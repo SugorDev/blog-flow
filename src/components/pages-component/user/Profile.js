1ed8c7793994b2c2372f38fa44f04d007bffcf3c
Joaquin Reynera <critizize.joaquin@gmail.com>
2023-05-21 05:37:13 +0800

    user-profile section design complete

--- src/components/pages-component/user/Profile.js
+++ src/components/pages-component/user/Profile.js
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
 import './profile-style.css';
 import Navbar from "../sub-components/Navbar";
-
+import UserPosts from './user-sub-components/UserPosts';
+import AboutBlogFlow from '../sub-components/AboutBlogFlow';
 
 export default function UserProfile(){
     const userData = {
             'profile-pic' : 'https://cdn.discordapp.com/attachments/773993070373896284/1079458247003623535/received_535769868498611.jpg',
         }
+    
+    
     return (
         <>
             <Navbar />
@@ -20,7 +24,37 @@ export default function UserProfile(){
                         Hi there! I'm [Your Name], a passionate blogger and storyteller with a love for sharing inspiring content with the world. I believe that every person has a unique story to tell, and through my blog, I aim to create a space where those stories can be celebrated.
                     </div>
                 </div>
+                <div className='user-main-body d-flex justify-content-center align-items-start'>
+                        <div className='user-social-links d-flex justify-content-start align-items-center flex-column'>
+                            <h3>Social Links</h3>
+                            <ul>
+                                <li>
+                                    <i className="fa-brands fa-facebook"></i>
+                                    <Link>John Doe</Link>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-instagram"></i>
+                                    <Link>@_itsmejohndoe</Link>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-twitter"></i>
+                                    <Link>@johndoe</Link>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-youtube"></i>
+                                    <Link>JohnDoeChannel</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='user-posts-section'>
+                            <h2> Author Posts </h2>
+                            <div className='user-posts-container'>
+                            <UserPosts />
+                            </div>
+                        </div>
+                </div>
             </div>
+            <AboutBlogFlow />
         </>
     );
 }
--- src/components/pages-component/user/user-sub-components/UserPosts.js
+++ src/components/pages-component/user/user-sub-components/UserPosts.js
@@ -1,10 +1,18 @@
-
-
-{/* will render posts of current logged in user */}
+import { Link } from 'react-router-dom';
+import data from '../sample-user-posts.json';
+import '../profile-style.css';
 
 export default function UserPosts(){
+    const { posts } = data;
+
     return (
-        <>
+        <>  
+            {posts.map((post, index) => (
+                <div key={index} className="user-post">
+                    <span>{post.title}</span>
+                    <Link to={'/blog-flow'}><div className='user-post-thumbnail' style={{'backgroundImage': `url(${post.thumbnail})`}}></div></Link>
+                </div>
+            ))}
         </>
     );
 }
