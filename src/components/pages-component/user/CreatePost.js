dbd415e6d19962c3633ff01344dd9844598d23f3
sugor <sugorer@gmail.com>
2023-05-22 19:38:43 +0800

    refactor creatposts-section

--- src/components/pages-component/user/CreatePost.js
+++ src/components/pages-component/user/CreatePost.js
@@ -1,29 +1,14 @@
-import Navbar from "../sub-components/Navbar";
-import React, { useState } from 'react';
 import './create-style.css';
+import Navbar from "../sub-components/Navbar";
+import AboutBlogFlow from '../sub-components/AboutBlogFlow';
 
 export default function CreatePost(){
-    const [title, setTitle] = useState('');
-    const [thumbnail, setThumbnail] = useState('');
-    const [about, setAbout] = useState('');
-    const [tags, setTags] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Here, you can perform any necessary actions with the blog data (e.g., submit to a server or update state)
-      console.log('Blog submitted:', { title, thumbnail, about, tags });
-      // Reset the form after submission
-      setTitle('');
-      setThumbnail('');
-      setAbout('');
-      setTags('');
-    };
-
     return (
         <>
             <Navbar />
             <div className="create-post-section">
-                <form className="create-form" onSubmit={handleSubmit}>
+                <h1>Create Your Blog</h1>
+                <form className="create-form">
                     <div className="mb-3">
                         <label for="form-blog-title" className="form-label">What is title of your blog?</label>
                         <input type="email" className="form-control" id="form-blog-title" required/>
@@ -34,12 +19,11 @@ export default function CreatePost(){
                     </div>
                     <div className="mb-3">
                         <label for="form-blog-content" className="form-label">What is the Blog about?</label>
-                        <textarea className="form-control" id="form-blog-content" rows="3" required></textarea>
+                        <textarea className="form-control" id="form-blog-content" rows="15" required></textarea>
                     </div>
                     <div className="input-group mb-3">
-                        <label className="input-group-text" for="form-blog-tags">Options</label>
+                        <label className="input-group-text" for="form-blog-tags">Tags</label>
                         <select className="form-select" id="form-blog-tags">
-                            <option selected>Choose...</option>
                             <option value="Lifestyle">Lifestyle</option>
                             <option value="Fashion">Fashion</option>
                             <option value="Technology">Technology</option>
@@ -47,9 +31,10 @@ export default function CreatePost(){
                             <option value="Health">Health</option>
                         </select>
                     </div>
-                    <button type="submit">Create Blog</button>
+                    <button className='create-blog-btn' type="submit">Create Blog</button>
                 </form>
             </div>
+            <AboutBlogFlow />
         </>
     );
 }
